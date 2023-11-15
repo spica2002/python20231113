37e4cd7199a1cfbc7b7d7f6eb25848fa23ba008f
spica2002 <youngho.shin@gmail.com>
2023-11-15 10:11:28 +0900

    파일 처리 연습 1

---
+++ DemoFormat.py
@@ -0,0 +1,19 @@
+# DemoFormat.py
+
+for i in range(1,11):
+  URL = "https://multi.com/?page=" + str(i)
+  print(URL)
+
+print("---숫자 출력---")
+for x in range(1,6):
+  print(x,"*",x,"=",x*x)
+
+print("---오른쪽 정렬---")
+for x in range(1,6):
+  print(x,"*",x,"=",str(x*x).rjust(3))
+
+print("---0으로 채우기---")
+for x in range(1,6):
+  print(x,"*",x,"=",str(x*x).zfill(5))
+
+  
